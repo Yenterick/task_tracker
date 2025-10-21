9bc0e874316b77860c6629ec54d1df9420f84d14
Juan David Martínez <yenterick@gmail.com>
2025-10-21 15:26:40 -0500

    Full task menu visual implemented

--- frontend/src/pages/TaskMenu/TaskMenu.jsx
+++ frontend/src/pages/TaskMenu/TaskMenu.jsx
@@ -20,6 +20,18 @@ function TaskMenu() {
     fetchTasks();
   }, [tasks]);
 
+  const taskExamples = [{title: "Test Task 1", description: "Test Description 1"}, 
+    {title: "Test Task 2", description: "Test Description 2"},
+    {title: "Test Task 3", description: "Test Description 3"},
+    {title: "Test Task 4", description: "Test Description 4"},
+    {title: "Test Task 5", description: "Test Description 5"},
+    {title: "Test Task 6", description: "Test Description 6"},
+    {title: "Test Task 7", description: "Test Description 7"},
+    {title: "Test Task 8", description: "Test Description 8"},
+    {title: "Test Task 9", description: "Test Description 9"},
+    {title: "Test Task 10", description: "Test Description 10"}
+  ]
+
   return (
     <div className="taskMenu-page">
       <div className="background">
@@ -36,17 +48,34 @@ function TaskMenu() {
       </div>
 
       <div className="task-list">
-            <AnimatedList enableArrowNavigation displayScrollbar items={tasks.map((task, id) => (
-                        <div key={id} className="task-container">
-                            <h3>{task.title}</h3>
-                            <p>{task.description}</p>
-                            <button className="button">Edit</button>
-                            <button className="button">Delete</button>
-                        </div>
-                    ))}/>
+        <AnimatedList showGradients={false} enableArrowNavigation displayScrollbar items={taskExamples.map((task, id) => (
+          <div key={id} className="task-container">
+            <div className="task-text">
+              <h2>{task.title}</h2>
+              <p>{task.description}</p>
+            </div>
+            <div className="task-buttons">
+              <button className="button"><i class="fa-solid fa-pencil"></i></button>
+              <button className="button"><i class="fa-solid fa-trash"></i></button>
+            </div>
+          </div>
+        ))}/>
       </div>
+
+      <button id="add-task" className="button">+</button>
+      <button className="log-out"><i class="fa-solid fa-right-from-bracket"></i></button>
+      
     </div>
   );
 }
 
 export default TaskMenu;
+
+// items={tasks.map((task, id) => (
+//                         <div key={id} className="task-container">
+//                             <h3>{task.title}</h3>
+//                             <p>{task.description}</p>
+//                             <button className="button">Edit</button>
+//                             <button className="button">Delete</button>
+//                         </div>
+//                     ))}
